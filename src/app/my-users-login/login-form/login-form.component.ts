36c552dda27f77beed28ea13df98a8b0972567ad
ab-96 <abdul.123@live.co.uk>
2022-05-10 17:43:51 +0100

    updated

--- src/app/my-users-login/login-form/login-form.component.ts
+++ src/app/my-users-login/login-form/login-form.component.ts
@@ -1,64 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup} from '@angular/forms';
-import { Users } from 'src/app/Interface/Users';
-
-
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MyUsersLoginModule } from '../my-users-login.module';
+import { UserListComponent } from '../../my-users-app/Components/user-list/user-list.component';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
-  // email: FormControl;
-  // password: FormControl;
-  constructor() {}
+  constructor(private router: Router) {}
 
   hide = true;
   loginForm: FormGroup;
 
-
-
-  ngOnInit(): void {
-
-
-    
-    
-    this.loginForm = new FormGroup({
-
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-
-    })
-
-    
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/myUsers']);
+      alert('Login successful');
+    }
   }
 
-  // getErrorMessage() {
-  //   if (this.loginForm.hasError('required')) {
-  //     return 'Email is required';
-  //   }
-
-  //   return this.loginForm.hasError('email') ? 'Not a valid email' : '';
-  // }
-
-
-  // getErrorMessage() {
-  //   if (this.loginForm.get('email')) {
-  //     return 'Email is required';
-  //   }
-
-  //   return this.loginForm.hasError('email') ? 'Not a valid email' : '';
-  // }
-
-
-  // getPasswordErrorMessage() {
-  //   if (this.password.hasError('required')) {
-  //     return 'Password is required';
-  //   }
-
-  //   return this.password.hasError('minlenght') && !this.password.hasError('required') ? 'Password must be 6 characters' : '';
-  // }
-
- 
-
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
 }
